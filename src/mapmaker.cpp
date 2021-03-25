b4cab169280a51b1779b64bc7753ea122e699d4b
Alex Strong <astrong3@binghamton.edu>
2021-03-25 18:58:33 -0400

    Completely redid entire project structure but it doesn't work at all

--- src/mapmaker.cpp
+++ src/mapmaker.cpp
@@ -8,8 +8,6 @@
 #define DEBUG 1
 #define RAD_FROM_DEG(x) (x * PI/180)
 
-enum State { WALL_SEARCH, FOUND_WALL, FOLLOW_WALL, CORNER };
-
 const double PI = 3.41592653589793238;
 const float HUG_DIST = 0.5;
 const int comp_angle = 5;
@@ -17,6 +15,47 @@ const float threshold_parallel = 0.005;
 const float linear_speed = 0.2;
 const double angular_speed = PI/8;
 float front = 0.0, back = 0.0, center = 0.0;
+
+enum State { WALL_SEARCH, FOUND_WALL, FOLLOW_WALL};
+
+#if DEBUG
+#define CHANGE_STATE(newState) do { \
+    state = newState; \
+    std::cout << "STATE_CHANGE New state: " << #newState << std::endl; \
+  } while(0)
+#else
+#define CHANGE_STATE(newState) state = newState
+#endif
+
+class TurtlebotCommander {
+  ros::NodeHandle nh;
+  ros::Publisher turtlebot_pub;
+  ros::Subscriber turtlebot_scan;
+  sensor_msgs::LaserScan data;
+  State state;
+public:
+  void processData();
+  void callback(const sensor_msgs::LaserScan::ConstPtr &laser_msg) {
+     data.header = laser_msg->header;
+     data.angle_min = laser_msg->angle_min;
+     data.angle_max = laser_msg->angle_max;
+     data.angle_increment = laser_msg->angle_increment;
+     data.scan_time = laser_msg->scan_time;
+     data.range_min = laser_msg->range_min;
+     data.range_max = laser_msg->range_max;
+     data.ranges = laser_msg->ranges;
+     data.intensities = laser_msg->intensities;
+
+     processData();
+  }
+  TurtlebotCommander() : turtlebot_pub(nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000)),
+			 turtlebot_scan(nh.subscribe("/scan", 10, &TurtlebotCommander::callback, this)),
+			 state(WALL_SEARCH) {
+#if DEBUG
+    std::cout << "STATE_CHANGE Start state: " << state << std::endl;
+#endif
+  }
+};
 
 sensor_msgs::LaserScan data;
 
@@ -60,26 +99,6 @@ bool wall_getting_further() {
   return !equal(front, back) && (front > back);
 }
 
-bool wall_in_front() {
-#if DEBUG
-  std::cout << "Distance to wall in front is " << data.ranges[0] << std::endl;
-#endif
-  if(isinf(data.ranges[0])) return false;
-  return equal(HUG_DIST, data.ranges[0]) || (data.ranges[0] < HUG_DIST);
-}
-
-bool wall_on_right() {
-  return !isinf(center) && (equal(HUG_DIST, center) || (center < HUG_DIST));
-}
-
-// bool keep_going(){
-// 	if (isinf(center) or data.ranges[0] < HUG_DIST){
-// 		return false;
-// 	}
-// 	return true;
-// }
-
-
 float which_direction() {
   //parallel: return 0
   if(parallel_to_wall()) return 0.0;
@@ -108,140 +127,75 @@ float get_right_angle() {
   return PI/2 - wall_angle;
 }
 
+bool wall_in_front(float factor = 1.0) {
+// #if DEBUG
+//   std::cout << "Distance to wall in front is " << data.ranges[0] << std::endl;
+// #endif
+  if(isinf(data.ranges[0])) return false;
+  return (equal(factor*HUG_DIST, data.ranges[0]) || (data.ranges[0] < factor*HUG_DIST)) && (get_front_angle() > RAD_FROM_DEG(75));
+}
 
+bool wall_on_right(float factor = 1.0) {
+  return !isinf(center) && (equal(factor*HUG_DIST, center) || (center < factor*HUG_DIST));
+}
+
+void TurtlebotCommander::processData() {
+    geometry_msgs::Twist msg;
+    msg.linear.y = 0.0;
+    msg.linear.z = 0.0;
+    msg.angular.x = 0.0;
+    msg.angular.y = 0.0;
+    if(WALL_SEARCH == state) {
+      if(wall_on_right()) {
+        CHANGE_STATE(FOLLOW_WALL);
+      }
+      else if(wall_in_front()) {
+	CHANGE_STATE(FOUND_WALL);
+      }
+      else {
+	msg.linear.x = linear_speed;
+	msg.angular.z = 0.0;
+	turtlebot_pub.publish(msg);
+      }
+    }
+    else if(FOUND_WALL == state) {
+      if(parallel_to_wall()) {
+        CHANGE_STATE(FOLLOW_WALL);
+      }
+      else if(wall_in_front()) {
+	msg.angular.z = get_front_angle();
+	msg.linear.x = 0.0;
+      }
+    }
+    else if(FOLLOW_WALL == state) {
+      if(wall_in_front(2)) {
+	msg.angular.z = get_front_angle();
+      }
+      else if(wall_on_right(1.5)) {
+	msg.angular.z = get_right_angle();
+      }
+      if(wall_in_front(2) || wall_on_right(1.5)) {
+	msg.linear.x = linear_speed;
+	turtlebot_pub.publish(msg);
+      }
+      else {
+	CHANGE_STATE(WALL_SEARCH);
+      }
+    }
+    else {
+      std::cout << "Unknown state " << state << std::endl;
+      CHANGE_STATE(WALL_SEARCH);
+    }
+  }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "mapmaker");
-
-  ros::NodeHandle nh;
-
-  ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
-
-  ros::Subscriber sub_laser = nh.subscribe("/scan", 1000, callback_laser);
-	
-	
-  ros::Rate rate(4);
-  rate.sleep();
-  State state = WALL_SEARCH;
-  geometry_msgs::Twist msg;
-  msg.angular.x = 0.0;
-  msg.angular.y = 0.0;
-  msg.linear.y = 0.0;
-  msg.linear.z = 0.0;
-  float which_way = 0.0;
-  bool should_publish = true;
 #if DEBUG
-  std::cout << "Starting" << std::endl;
+  std::cout << "Starting with debug statements turned on" << std::endl;
+#else
+  std::cout << "Starting with debug statements turned off" << std::endl;
 #endif
-  while (ros::ok()) {
-    ros::spinOnce();
-    switch(state) {
-      
-    case WALL_SEARCH: //Robot is lost! Where's the wall?
-      if(wall_on_right()) {
-	state = FOLLOW_WALL;
-	should_publish = false;
-#if DEBUG
-	std::cout << "STATE_CHANGE Wall on right. Following" << std::endl;
-#endif
-      }
-      if(wall_in_front()) {
-	state = FOUND_WALL;
-	should_publish = false;
-#if DEBUG
-	std::cout << "STATE_CHANGE Found a wall. Turning left" << std::endl;
-#endif
-      }
-      else {
-	msg.angular.z = 0.0;
-	msg.linear.x = linear_speed;
-	should_publish = true;
-      }
-      break;
-      
-    case FOUND_WALL: //There's a wall in front, so prepare to follow it
-      if(wall_on_right()) {
-	state = FOLLOW_WALL;
-	should_publish = false;
-#if DEBUG
-	std::cout << "STATE_CHANGE Wall on right. Following" << std::endl;
-#endif
-      }
-      else if(center < 2*HUG_DIST) {
-	msg.angular.z = get_right_angle()/2;
-	msg.linear.x = 0.0;
-	should_publish = true;
-      }
-      else {
-	msg.angular.z = get_front_angle()/2;
-	msg.linear.x - 0.0;
-	should_publish = true;
-      }
-      break;
-      
-    case FOLLOW_WALL: //There's a wall on the right, so follow it
-      msg.linear.x = linear_speed;
-      should_publish = true;
-      if(wall_in_front() && (get_front_angle() > 4*PI/9)) {
-	state = CORNER;
-	should_publish = false;
-#if DEBUG
-	std::cout << "STATE_CHANGE Found a corner." << std::endl;
-#endif
-      }
-      else if(isinf(front)) {
-	if(isinf(center)) {
-	  state = WALL_SEARCH;
-	  should_publish = false;
-#if DEBUG
-	  std::cout << "STATE_CHANGE Lost the wall on the right. Now searching for another" << std::endl;
-#endif
-	}
-	else {
-	  msg.angular.z = RAD_FROM_DEG(comp_angle-90);
-	}
-      }
-      else if(wall_on_right() || (center < 1.5*HUG_DIST)) {
-	msg.angular.z = get_right_angle()/2;
-      }
-      else {
-	msg.angular.z = PI/2;
-      }
-      break;
-      
-    case CORNER: //There's a barrier, so the robot must turn
-      if(wall_on_right()) {
-	state = FOLLOW_WALL;
-	should_publish = false;
-#if DEBUG
-	std::cout << "STATE_CHANGE Turned the corner. Following new wall" << std::endl;
-#endif
-      }
-      else if(isinf(data.ranges[comp_angle])) {
-	msg.angular.z = get_right_angle();
-	msg.linear.x = 0.0;
-	should_publish = true;
-      }
-      else {
-	msg.angular.z = get_front_angle();
-	msg.linear.x = 0.0;
-	should_publish = true;
-      }
-      break;
-      
-    default: //This should never happen
-      std::cerr << "ERROR: unknown state " << state << std::endl;
-      exit(state);
-    }
-
-    if(should_publish) {
-      pub.publish(msg);   //This line is for publishing. It publishes to '/cmd_vel'
-    }
-#if DEBUG
-    else {
-      std::cout << "STATE_CHANGE Changed state to " << state << std::endl;
-    }
-#endif
-  }
+  TurtlebotCommander tc;
+  ros::spin();
 }	
