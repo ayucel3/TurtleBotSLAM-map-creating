d404acd588d801369fa09a779fbd7e06d371cbc0
ayucel3 <ayucel3@binghamton.edu>
2021-03-26 06:27:34 +0300

    If we can manage turning angles this can work

--- src/mapmaker.cpp
+++ src/mapmaker.cpp
@@ -8,7 +8,7 @@
 #define DEBUG 1
 #define RAD_FROM_DEG(x) (x * PI/180)
 
-enum State { WALL_SEARCH, FOUND_WALL, FOLLOW_WALL, CORNER };
+enum State { WALL_SEARCH, FOLLOW_WALL, TURN_RIGHT ,START , TURN_LEFT};
 
 const double PI = 3.41592653589793238;
 const float HUG_DIST = 0.5;
@@ -20,15 +20,6 @@ float front = 0.0, back = 0.0, center = 0.0;
 
 sensor_msgs::LaserScan data;
 
-//I DID NOT USE THIS since the sensors are not giving exactly what we want instead I used a threshold
-bool equal(float a, float b) {
-  float threshold = 0.995;
-  if(0.0 != b) {
-    float ratio = a/b;
-    return (ratio > threshold) && (ratio < 1/threshold);
-  }
-  return abs(a) < 1/threshold - 1;
-}
 
 //Taking the vision info
 void callback_laser(const sensor_msgs::LaserScan::ConstPtr& laser_msg)
@@ -47,62 +38,24 @@ void callback_laser(const sensor_msgs::LaserScan::ConstPtr& laser_msg)
   center = data.ranges[270];
 }
 
-bool parallel_to_wall() {
-  return equal(front, back) || equal(cos(RAD_FROM_DEG(comp_angle)), center/front) ||
-    equal(cos(RAD_FROM_DEG(comp_angle)), center/back);
+bool there_is_wall(){
+	if(!isinf(data.ranges[0])){return true;}
+	return false;
 }
 
-bool wall_getting_closer() {
-  return !equal(front, back) && (front < back);
+bool wall_found(){
+	if(data.ranges[0] < HUG_DIST){
+		return true;
+	}
+	return false;
 }
 
-bool wall_getting_further() {
-  return !equal(front, back) && (front > back);
+bool wall_on_right(){
+ if(isinf(center) or (center > HUG_DIST + 0.05) ){//to have some upper limit biger than hug dist to give some capability to bot
+	return false;
+ }
+ return true;
 }
-
-bool wall_in_front() {
-  if(isinf(data.ranges[0])) return false;
-  return equal(HUG_DIST, data.ranges[0]) || (data.ranges[0] < HUG_DIST);
-}
-
-bool wall_on_right() {
-  return !isinf(center) && (equal(HUG_DIST, center) || (center < HUG_DIST));
-}
-
-// bool keep_going(){
-// 	if (isinf(center) or data.ranges[0] < HUG_DIST){
-// 		return false;
-// 	}
-// 	return true;
-// }
-
-
-float which_direction() {
-  //parallel: return 0
-  if(parallel_to_wall()) return 0.0;
-  if(isinf(front) && isinf(center) && isinf(back)) return 0.0; 
-  if(isinf(front)) return 1.0;
-  //wall turns left: return <0
-  //wall turns right: return >0
-  return front - back;
-}
-
-float get_front_angle() {
-  if(isinf(data.ranges[0])) return 0;
-  if(isinf(data.ranges[comp_angle])) return RAD_FROM_DEG(comp_angle);
-  float wall_dist = sqrt(pow(data.ranges[0],2) + pow(data.ranges[comp_angle],2) - 2*data.ranges[0]*data.ranges[comp_angle]*cos(RAD_FROM_DEG(comp_angle)));
-  float turn_angle = acos((pow(wall_dist, 2) + pow(data.ranges[0],2) - pow(data.ranges[comp_angle],2)) / (2*wall_dist*data.ranges[0]));
-  return PI - turn_angle;
-}
-
-float get_right_angle() {
-  if(isinf(data.ranges[0])) return RAD_FROM_DEG(270);
-  if(isinf(data.ranges[270+comp_angle])) return RAD_FROM_DEG(270+comp_angle);
-  float wall_dist = sqrt(pow(front,2) + pow(center,2) - 2*front*center*cos(RAD_FROM_DEG(comp_angle)));
-  float wall_angle = acos((pow(wall_dist,2) + pow(center,2) - pow(front,2))/(2*wall_dist*center));
-  return PI/2 - wall_angle;
-}
-
 
 
 int main(int argc, char **argv)
@@ -118,125 +71,106 @@ int main(int argc, char **argv)
 	
   ros::Rate rate(4);
   rate.sleep();
-  State state = WALL_SEARCH;
+
+  ros::spinOnce();
+
+  State state = START;
   geometry_msgs::Twist msg;
+
   msg.angular.x = 0.0;
   msg.angular.y = 0.0;
   msg.linear.y = 0.0;
   msg.linear.z = 0.0;
   float which_way = 0.0;
-  bool should_publish = true;
+  bool searching_state = false;
+  bool corner = false;
+
 #if DEBUG
   std::cout << "Starting" << std::endl;
 #endif
+
   while (ros::ok()) {
-    ros::spinOnce();
     switch(state) {
-      
-    case WALL_SEARCH: //Robot is lost! Where's the wall?
-      if(wall_on_right()) {
-	state = FOLLOW_WALL;
-	should_publish = false;
-#if DEBUG
-	std::cout << "STATE_CHANGE Wall on right. Following" << std::endl;
-#endif
-      }
-      if(wall_in_front()) {
-	state = FOUND_WALL;
-	should_publish = false;
-#if DEBUG
-	std::cout << "STATE_CHANGE Found a wall. Turning left" << std::endl;
-#endif
-      }
-      else {
-	msg.angular.z = 0.0;
-	msg.linear.x = linear_speed;
-	should_publish = true;
-      }
-      break;
-      
-    case FOUND_WALL: //There's a wall in front, so prepare to follow it
-      if(wall_on_right()) {
-	state = FOLLOW_WALL;
-	should_publish = false;
-#if DEBUG
-	std::cout << "STATE_CHANGE Wall on right. Following" << std::endl;
-#endif
-      }
-      else if(center < 2*HUG_DIST) {
-	msg.angular.z = get_right_angle();
-	msg.linear.x = 0.0;
-	should_publish = true;
-      }
-      else {
-	msg.angular.z = get_front_angle();
-	msg.linear.x - 0.0;
-	should_publish = true;
-      }
-      break;
-      
-    case FOLLOW_WALL: //There's a wall on the right, so follow it
-      msg.linear.x = linear_speed;
-      should_publish = true;
-      if(wall_in_front()) {
-	state = CORNER;
-	should_publish = false;
-#if DEBUG
-	std::cout << "STATE_CHANGE Found a corner." << std::endl;
-#endif
-      }
-      else if(isinf(front)) {
-	if(isinf(center)) {
-	  state = WALL_SEARCH;
-	  should_publish = false;
-#if DEBUG
-	  std::cout << "STATE_CHANGE Lost the wall on the right. Now searching for another" << std::endl;
-#endif
+
+    case START: //JUST TO FIND THE FIRST WALL THERE IS GAP ON THE RIGHT SIDE WHICH thats why needed to seperate inital state after that we ll hopefully have the wall on our right side 
+	if(wall_found()){
+		state = WALL_SEARCH;
+		searching_state = false;
 	}
-	else {
-	  msg.angular.z = RAD_FROM_DEG(comp_angle-90);
+	else{
+		msg.angular.z = 0;
+		msg.linear.x = linear_speed;
 	}
-      }
-      else if(wall_on_right() || (center < 1.5*HUG_DIST)) {
-	msg.angular.z = get_right_angle();
-      }
-      else {
-	msg.angular.z = PI/2;
-      }
+
+        break;
+      
+    case WALL_SEARCH: //MAIN STATEMENT robot always trys to move forward and if there is wall stops to turn and if the right side is not a wall calls turn right
+	if(wall_found()){
+		msg.angular.z = angular_speed;
+		msg.linear.x = 0;
+		state = FOLLOW_WALL;
+	}
+	else if(!wall_on_right()){//This is to be able to turn right when there is no wall but we can not do this when we start because chairs on the right gives inf at the start so agent goes crazy thats why we needed start state to just move the agent to wall directly.
+		state = TURN_RIGHT;
+		
+	}
+	else{
+		msg.angular.z = 0;
+		msg.linear.x = linear_speed;
+	}
+
+        break;
+
+      
+    case FOLLOW_WALL: //THIS Statement is just for getting sleep time and detecting corners when WALL_SEARCH detects there is a wall a head
+	if(wall_on_right() and wall_found()){ //for corner
+		sleep(1);
+		state = TURN_LEFT;
+		corner = true;
+	}
+	else if(wall_on_right()){ //for just a side
+		state = WALL_SEARCH;
+	}
+	else{
+		sleep(1);
+	}
       break;
       
-    case CORNER: //There's a barrier, so the robot must turn
-      if(wall_on_right()) {
-	state = FOLLOW_WALL;
-	should_publish = false;
-#if DEBUG
-	std::cout << "STATE_CHANGE Turned the corner. Following new wall" << std::endl;
-#endif
-      }
-      else if(isinf(data.ranges[comp_angle])) {
-	msg.angular.z = get_right_angle();
-	msg.linear.x = 0.0;
-	should_publish = true;
-      }
-      else {
-	msg.angular.z = get_front_angle();
-	msg.linear.x = 0.0;
-	should_publish = true;
-      }
+    case TURN_RIGHT: //IF we are in searching state which means the right wall is lost or too far it will enter the first if and searching state will be set to true and also it will start turning to right
+	if(!wall_on_right()){
+		if(searching_state){// This code only runs once when it is in search state sleep is used to be able to turn the agent 
+			sleep(1);
+			state = START;
+			
+		}
+		msg.angular.z = -angular_speed*2;
+		msg.linear.x = 0;
+		searching_state = true;
+	}
+	else{
+		sleep(1);
+		state = WALL_SEARCH;//THis part is to set agent to normal state which is going forward and finding walls
+	}
       break;
-      
+
+    case TURN_LEFT: //This is to handle corners
+	if(corner){
+		msg.angular.z = angular_speed;
+		msg.linear.x = 0;
+		corner = false;
+	}
+	else{
+		sleep(1);
+		state = WALL_SEARCH;
+	}
+      break;
+
     default: //This should never happen
       std::cerr << "ERROR: unknown state " << state << std::endl;
       exit(state);
     }
-
-    if(should_publish) {
-      pub.publish(msg);   //This line is for publishing. It publishes to '/cmd_vel'
-    }
-#if DEBUG
-    else {
-      std::cout << "STATE_CHANGE Changed state to " << state << std::endl;
-    }
-#endif
+   rate.sleep();
+   ros::spinOnce();
+   pub.publish(msg);
   }
 }	
